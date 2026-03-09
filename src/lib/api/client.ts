ce436589d9d3fbf77cc2f8e20c7c3cb3c09a2c2a
yoiner castillo <jhoicas@gmail.com>
2026-03-08 23:31:58 -0500

    apuntando a nuevo subdominio

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import { AUTH_TOKEN_COOKIE_KEY } from "@/config/auth";
 
-const API_BASE_URL = "https://api.ludoia.com";
+const API_BASE_URL = "https://api.NaturERP.ludoia.com";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
